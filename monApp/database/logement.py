999ec40ece86cdaefa74b9815e3a56f87309ec82
clement-vgc <clement.vignon-chaudey@etu.univ-orleans.fr>
2025-12-15 09:13:52 +0100

    correction message d'erreur BD

--- monApp/database/logement.py
+++ monApp/database/logement.py
@@ -3,7 +3,6 @@ from monApp.database.justifie import justifie
 from monApp.database.couvre import couvre
 from monApp.database.possede import possede
 
-
 class Logement(db.Model):
     __tablename__ = 'logement'
     id_logement = db.Column(db.Integer, primary_key=True)
@@ -15,8 +14,7 @@ class Logement(db.Model):
     pieces = db.relationship('Piece', backref='logement', lazy='subquery', cascade="all, delete-orphan")
     sinistres = db.relationship('Sinistre', backref='logement', lazy=True)
     justificatifs = db.relationship('Justificatif', secondary=justifie, backref=db.backref('logements', lazy='subquery'), lazy='subquery')
-    assureurs = db.relationship('Assureur', secondary=couvre, backref=db.backref('logements', lazy='subquery'), lazy='subquery')
-
+    assureurs = db.relationship('Assureur', secondary=couvre, back_populates='logements_couverts', lazy='subquery')
 
     def __init__(self, adresse, type_logement=None, surface=None, description=None):
         self.adresse = adresse
